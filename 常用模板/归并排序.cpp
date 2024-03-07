965c563ca75701eb4e5e691b22a9d7e188dc0076
DNX <109335896+dnx00@users.noreply.github.com>
2024-03-07 22:50:12 +0800

    Create 模板_归并排序

--- 常用模板/归并排序.cpp
+++ 常用模板/归并排序.cpp
@@ -1,10 +1,9 @@
-// 归并排序
-// Created by DNX on 2024/2/27.
+// 模板_归并排序
+// Created by DNX on 2024/3/7.
 //
 #include <iostream>
-#include <cstdio>
 
-#define N 10010
+#define N 100010
 
 using namespace std;
 
@@ -14,38 +13,42 @@ int n;
 int arr[N];
 int tmp[N];
 
-int main() {
+int main()
+{
     cin >> n;
-    for (int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
 
-    mergeSort(0, n - 1);
+    mergeSort(0, n-1);
 
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << ' ';
+    cout << endl;
 
     return 0;
 }
 
-void mergeSort(int l, int r) {
-    if (l >= r)
+void mergeSort(int l, int r)
+{
+    if(l >= r)
         return;
 
     int mid = (l + r) >> 1;
     mergeSort(l, mid);
-    mergeSort(mid + 1, r);
+    mergeSort(mid+1, r);
 
-    int k = 0, i = l, j = mid + 1;
-    while (i <= mid && j <= r) {
-        if (arr[i] <= arr[j])
+    int k = 0, i = l, j = mid+1;
+    while(i <= mid && j <= r)
+        if(arr[i] <= arr[j])
             tmp[k++] = arr[i++];
         else
             tmp[k++] = arr[j++];
-    }
-    while (i <= mid)
+
+    while(i <= mid)
         tmp[k++] = arr[i++];
-    while (j <= r)
+    while(j <= r)
         tmp[k++] = arr[j++];
-    for (i = l, j = 0; i <= r; i++, j++)
+
+    for(i = l, j = 0; i <= r; i++, j++)
         arr[i] = tmp[j];
 }
