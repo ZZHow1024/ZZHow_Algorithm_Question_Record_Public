bd5438a05f19c2ee056254c261fcd13c7e571fe9
DNX <109335896+dnx00@users.noreply.github.com>
2024-03-14 23:17:48 +0800

    Create dfs_以排列数字为例

---
+++ 常用模板/dfs_以排列数字为例.cpp
@@ -0,0 +1,45 @@
+// dfs_以排列数字为例
+// 842. 排列数字
+// Created by DNX on 2024/3/14.
+//
+#include <iostream>
+
+#define N 10
+
+using namespace std;
+
+void dfs(int u);
+
+int n;
+int path[N];
+bool st[N];
+
+int main()
+{
+    cin >> n;
+
+    dfs(0);
+
+    return 0;
+}
+
+void dfs(int u)
+{
+    if(u == n)
+    {
+        for(int i = 0; i < n; i++)
+            cout << path[i] << ' ';
+        cout << endl;
+        return;
+    }
+
+    for(int i = 1; i <= n; i++)
+        if(!st[i])
+        {
+            path[u] = i;
+            st[i] = true;
+            dfs(u + 1);
+            //恢复现场
+            st[i] = false;
+        }
+}
