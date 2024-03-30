9c3668fea70f23952c368eb81c9c238956b3dbcc
DNX <109335896+dnx00@users.noreply.github.com>
2024-03-30 20:25:03 +0800

    Create A_5561. 棋子数量

---
+++ AcWing周赛/第 149 场周赛/A_5561. 棋子数量.cpp
@@ -0,0 +1,25 @@
+// A_5561. 棋子数量
+// Created by DNX on 2024/3/30.
+//
+#include <iostream>
+
+#define N 110
+
+using namespace std;
+
+int n;
+int tmp;
+int ans;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        if (tmp == 1)
+            ++ans;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
